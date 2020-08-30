32e0987f5030d3858189a717d13f0493d6cdfa81
Mehul Mohan <mehulmpt@gmail.com>
2020-08-30 20:03:11 +0530

    lab changed

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
-module.exports = () => {
-	let str = 'one two three'
-	let fixRegex = /change/ // Change this line
-	let replaceText = '' // Change this line
-	let result = str.replace(fixRegex, replaceText)
-
-	return result
-}
+let str = 'one two three'
+let fixRegex = /change/ // Change this line
+let replaceText = '' // Change this line
+let result = str.replace(fixRegex, replaceText)
