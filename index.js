aea147ba70f928119c2cd84922713783f9a0fa25
Mehul Mohan <mehulmpt@gmail.com>
2020-08-30 20:06:35 +0530

    lab changed

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-module.exports = () => {
-	let quoteSample = 'The quick brown fox jumps over the lazy dog.'
-	let alphabetRegex = /change/ // Change this line
-	let result = alphabetRegex // Change this line
-
-	return result
-}
+let quoteSample = 'The quick brown fox jumps over the lazy dog.'
+let alphabetRegex = /change/ // Change this line
+let result = alphabetRegex // Change this line
