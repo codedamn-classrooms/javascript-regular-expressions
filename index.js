12e098d9dba2979f3803853297e20ba8b50649db
Mehul Mohan <mehulmpt@gmail.com>
2020-08-30 20:06:15 +0530

    lab changed

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-module.exports = () => {
-	let difficultSpelling = 'Mississippi'
-	let myRegex = /change/ // Change this line
-	let result = difficultSpelling.match(myRegex)
-
-	return result
-}
+let difficultSpelling = 'Mississippi'
+let myRegex = /change/ // Change this line
+let result = difficultSpelling.match(myRegex)
