fdaa5eb95dd59a8284cfbd29297bb4f6d919f785
Mehul Mohan <mehulmpt@gmail.com>
2020-08-30 20:07:03 +0530

    lab changed

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-module.exports = () => {
-	let extractStr = "Extract the word 'coding' from this string."
-	let codingRegex = /change/ // Change this line
-	let result = extractStr // Change this line
-
-	return result
-}
+let extractStr = "Extract the word 'coding' from this string."
+let codingRegex = /change/ // Change this line
+let result = extractStr // Change this line
