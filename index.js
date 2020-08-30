b1a329b2afec7a6330eba76cfd5f04bbee68b61b
Mehul Mohan <mehulmpt@gmail.com>
2020-08-30 18:51:06 +0530

    Lab 1 sol added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 module.exports = () => {
 	let myString = 'Hello, World!'
 	let myRegex = /Hello/
-	let result = myRegex // Change this line
+	let result = myRegex.test(myString) // Change this line
 
 	return result
 }
