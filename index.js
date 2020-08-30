d332cf10283df913902c8f143b7be4a7a9939f1a
Mehul Mohan <mehulmpt@gmail.com>
2020-08-30 19:47:04 +0530

    lab added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 module.exports = () => {
-	let extractStr = "Extract the word 'coding' from this string."
-	let codingRegex = /change/ // Change this line
-	let result = extractStr // Change this line
+	let twinkleStar = 'Twinkle, twinkle, little star'
+	let starRegex = /change/ // Change this line
+	let result = twinkleStar // Change this line
 
 	return result
 }
