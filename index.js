b2fa9048177e1f2165f0db55f939abd21d179de5
Mehul Mohan <mehulmpt@gmail.com>
2020-08-30 20:07:30 +0530

    lab changed

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-module.exports = () => {
-	let myString = 'Hello, World!'
-	let myRegex = /Hello/
-	let result = myRegex // Change this line
-
-	return result
-}
+let myString = 'Hello, World!'
+let myRegex = /Hello/
+let result = myRegex // Change this line
