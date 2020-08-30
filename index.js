d55ac1008a84d16c1a6d5bf8ab810dd10881409f
Mehul Mohan <mehulmpt@gmail.com>
2020-08-30 20:03:21 +0530

    lab changed

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-module.exports = () => {
-	let repeatNum = '42 42 42'
-	let reRegex = /change/ // Change this line
-	let result = reRegex.test(repeatNum)
-
-	return result
-}
+let repeatNum = '42 42 42'
+let reRegex = /change/ // Change this line
+let result = reRegex.test(repeatNum)
