9e735efb03f62e2d763be81a873c51f0d9d8f98a
Mehul Mohan <mehulmpt@gmail.com>
2020-08-30 19:48:32 +0530

    lab added

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 module.exports = () => {
-	let quoteSample = 'Blueberry 3.141592653s are delicious.'
+	let quoteSample = '3 blind mice.'
 	let myRegex = /change/ // Change this line
 	let result = myRegex // Change this line
 
