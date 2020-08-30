440383936caa7214eba081bc62bfedbf49c2bccf
Mehul Mohan <mehulmpt@gmail.com>
2020-08-30 20:06:28 +0530

    lab changed

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-module.exports = () => {
-	let quoteSample = 'Blueberry 3.141592653s are delicious.'
-	let myRegex = /change/ // Change this line
-	let result = myRegex // Change this line
-
-	return result
-}
+let quoteSample = 'Blueberry 3.141592653s are delicious.'
+let myRegex = /change/ // Change this line
+let result = myRegex // Change this line
