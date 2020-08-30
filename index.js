d5ed66f1b6bbfa89b94a57951620dd05d21e63a3
Mehul Mohan <mehulmpt@gmail.com>
2020-08-30 20:07:17 +0530

    lab changed

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-module.exports = () => {
-	let petString = 'James has a pet cat.'
-	let petRegex = /change/ // Change this line
-	let result = petRegex.test(petString)
-
-	return result
-}
+let petString = 'James has a pet cat.'
+let petRegex = /change/ // Change this line
+let result = petRegex.test(petString)
