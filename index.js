6fb1e198efd3aac250266f7a6d17cf76c8bb3db1
Mehul Mohan <mehulmpt@gmail.com>
2020-08-30 20:06:56 +0530

    lab changed

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-module.exports = () => {
-	let twinkleStar = 'Twinkle, twinkle, little star'
-	let starRegex = /change/ // Change this line
-	let result = twinkleStar // Change this line
-
-	return result
-}
+let twinkleStar = 'Twinkle, twinkle, little star'
+let starRegex = /change/ // Change this line
+let result = twinkleStar // Change this line
