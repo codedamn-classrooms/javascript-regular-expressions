a712da368fcc17d838219c93a29a5210b3503713
Mehul Mohan <mehulmpt@gmail.com>
2020-08-30 19:51:08 +0530

    lab added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 module.exports = () => {
 	let movieName = '2001: A Space Odyssey'
-	let numRegex = /change/ // Change this line
-	let result = movieName.match(numRegex).length
+	let noNumRegex = /change/ // Change this line
+	let result = movieName.match(noNumRegex).length
 
 	return result
 }
