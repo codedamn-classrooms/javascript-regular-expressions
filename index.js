8ac51273176d1ed81574a4c6ed87768cf0370ad3
Mehul Mohan <mehulmpt@gmail.com>
2020-08-30 20:02:41 +0530

    lab added

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-module.exports = () => {
-	let hello = '   Hello, World!  '
-	let wsRegex = /change/ // Change this line
-	let result = hello // Change this line
-
-	return result
-}
+let hello = '   Hello, World!  '
+let wsRegex = /change/ // Change this line
+let result = hello // Change this line
