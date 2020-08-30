06b6ba763b36b8a93f9b5ca1ee04e6d7932b49fc
Mehul Mohan <mehulmpt@gmail.com>
2020-08-30 20:04:27 +0530

    lab changed

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-module.exports = () => {
-	let timStr = 'Timmmmber'
-	let timRegex = /change/ // Change this line
-	let result = timRegex.test(timStr)
-
-	return result
-}
+let timStr = 'Timmmmber'
+let timRegex = /change/ // Change this line
+let result = timRegex.test(timStr)
