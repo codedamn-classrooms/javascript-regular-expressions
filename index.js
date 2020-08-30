da22a842534e2ffb3310d6a187c63f97e64336a7
Mehul Mohan <mehulmpt@gmail.com>
2020-08-30 20:06:00 +0530

    lab changed

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-module.exports = () => {
-	let text = '<h1>Winter is coming</h1>'
-	let myRegex = /<.*>/ // Change this line
-	let result = text.match(myRegex)
-
-	return result
-}
+let text = '<h1>Winter is coming</h1>'
+let myRegex = /<.*>/ // Change this line
+let result = text.match(myRegex)
