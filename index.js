47ad06a364cbbb29c447b77c41598dd53787dcfa
Mehul Mohan <mehulmpt@gmail.com>
2020-08-30 19:49:26 +0530

    lab added

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 module.exports = () => {
-	let text = '<h1>Winter is coming</h1>'
-	let myRegex = /<.*>/ // Change this line
-	let result = text.match(myRegex)
+	let reCriminals = /./ // Change this line
 
 	return result
 }
