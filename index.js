ded965b9a369e077ad55daefde84a245eefce166
Mehul Mohan <mehulmpt@gmail.com>
2020-08-30 19:51:24 +0530

    lab added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 module.exports = () => {
-	let movieName = '2001: A Space Odyssey'
-	let noNumRegex = /change/ // Change this line
-	let result = movieName.match(noNumRegex).length
+	let username = 'JackOfAllTrades'
+	let userCheck = /change/ // Change this line
+	let result = userCheck.test(username)
 
 	return result
 }
