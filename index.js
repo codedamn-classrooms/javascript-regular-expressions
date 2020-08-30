a1efe21ecc18fb54c03ab7bae374d0d99efb55b8
Mehul Mohan <mehulmpt@gmail.com>
2020-08-30 20:04:36 +0530

    lab changed

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-module.exports = () => {
-	let haStr = 'Hazzzzah'
-	let haRegex = /change/ // Change this line
-	let result = haRegex.test(haStr)
-
-	return result
-}
+let haStr = 'Hazzzzah'
+let haRegex = /change/ // Change this line
+let result = haRegex.test(haStr)
