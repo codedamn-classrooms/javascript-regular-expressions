d14d4895fb13c76884fb458b01814dc68f7f6749
Mehul Mohan <mehulmpt@gmail.com>
2020-09-04 20:27:23 +0530

    solution added

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-let difficultSpelling = 'Mississippi'
-let myRegex = /s+/g // this is the solution
-let result = difficultSpelling.match(myRegex)
+let chewieQuote = 'Aaaaaaaaaaaaaaaarrrgh!'
+let chewieRegex = /Aa*/ // Change this line
+let result = chewieQuote.match(chewieRegex)
