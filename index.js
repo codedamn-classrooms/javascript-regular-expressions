316f60875d49d26d18d7e7ddc36aa831832110be
Mehul Mohan <mehulmpt@gmail.com>
2020-09-04 20:18:17 +0530

    solution added

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-let petString = 'James has a pet cat.'
-let petRegex = /dog|cat|bird|fish/ // Change this line
-let result = petRegex.test(petString)
+let myString = 'freeCodeCamp'
+let fccRegex = /freeCodeCamp/i // Change this line
+let result = fccRegex.test(myString)
