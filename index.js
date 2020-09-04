6e21fbcff438b7905f63756558ed512584bb430b
Mehul Mohan <mehulmpt@gmail.com>
2020-09-04 20:22:55 +0530

    solution added

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-let exampleStr = "Let's have fun with regular expressions!"
-let unRegex = /.un/ // Change this line
-let result = unRegex.test(exampleStr)
+let quoteSample = 'Beware of bugs in the above code; I have only proved it correct, not tried it.'
+let vowelRegex = /[aeiou]/gi // Change this line
+let result = quoteSample.match(vowelRegex) // Change this line
