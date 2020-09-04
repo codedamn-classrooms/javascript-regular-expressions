4ea2ca8dc1ed7c0371d792ab3929dbf0939ba1b3
Mehul Mohan <mehulmpt@gmail.com>
2020-09-04 20:29:54 +0530

    solution added

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-let text = '<h1>Winter is coming</h1>'
-let myRegex = /<.*?>/ // it's the answer!
-let result = text.match(myRegex)
+let crowd = 'P1P2P3P4P5P6CCCP7P8P9'
+
+let reCriminals = /C+/ // Change this line
+
+let matchedCriminals = crowd.match(reCriminals)
+console.log(matchedCriminals)
