6bd1591424fa77718274e2eb2f2327e7d3bbbdcd
Mehul Mohan <mehulmpt@gmail.com>
2020-09-04 20:24:08 +0530

    solution added

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-let quoteSample = 'The quick brown fox jumps over the lazy dog.'
-let alphabetRegex = /[a-z]/gi // Change this line
-let result = quoteSample.match(alphabetRegex) // Change this line
+let quoteSample = 'Blueberry 3.141592653s are delicious.'
+let myRegex = /[h-s2-6]/gi // Change this line
+let result = quoteSample.match(myRegex) // Change this line
