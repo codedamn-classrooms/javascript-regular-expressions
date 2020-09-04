b0a5e592ff2b9fe7323637523d664e8e511db89c
Mehul Mohan <mehulmpt@gmail.com>
2020-09-04 20:32:02 +0530

    solution added

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-let rickyAndCal = 'Cal and Ricky both like racing.'
-let calRegex = /^Cal/ // Change this line
-let result = calRegex.test(rickyAndCal)
+let caboose = 'The last car on a train is the caboose'
+let lastRegex = /caboose$/ // Change this line
+let result = lastRegex.test(caboose)
