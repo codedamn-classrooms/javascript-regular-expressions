f0a871696b2282a7bffd9d8fa893acf63c5b5fd6
Mehul Mohan <mehulmpt@gmail.com>
2020-09-04 20:30:56 +0530

    solution added

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-let crowd = 'P1P2P3P4P5P6CCCP7P8P9'
-
-let reCriminals = /C+/ // Change this line
-
-let matchedCriminals = crowd.match(reCriminals)
-console.log(matchedCriminals)
+let rickyAndCal = 'Cal and Ricky both like racing.'
+let calRegex = /^Cal/ // Change this line
+let result = calRegex.test(rickyAndCal)
