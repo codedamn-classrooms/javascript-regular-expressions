c6ffaae999f596131d245c65954a07d5f02fe906
Mehul Mohan <mehulmpt@gmail.com>
2020-09-04 20:32:39 +0530

    solution added

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-let caboose = 'The last car on a train is the caboose'
-let lastRegex = /caboose$/ // Change this line
-let result = lastRegex.test(caboose)
+let quoteSample = 'The five boxing wizards jump quickly.'
+let alphabetRegexV2 = /\w/g // Change this line
+let result = quoteSample.match(alphabetRegexV2).length
