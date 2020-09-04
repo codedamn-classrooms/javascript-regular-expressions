3db2d8d3569c7666b4cbd3b96370606e886be3ce
Mehul Mohan <mehulmpt@gmail.com>
2020-09-04 20:23:36 +0530

    solution added

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-let quoteSample = 'Beware of bugs in the above code; I have only proved it correct, not tried it.'
-let vowelRegex = /[aeiou]/gi // Change this line
-let result = quoteSample.match(vowelRegex) // Change this line
+let quoteSample = 'The quick brown fox jumps over the lazy dog.'
+let alphabetRegex = /[a-z]/gi // Change this line
+let result = quoteSample.match(alphabetRegex) // Change this line
