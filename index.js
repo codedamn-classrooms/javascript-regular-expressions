c0f9d5e1b2b3de72234d075053425b89490ef33c
Mehul Mohan <mehulmpt@gmail.com>
2020-09-04 20:16:50 +0530

    solution added

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-let myString = 'Hello, World!'
-let myRegex = /Hello/
-let result = myRegex.test(myString) // Change this line
+let waldoIsHiding = 'Somewhere Waldo is hiding in this text.'
+let waldoRegex = /Waldo/ // Change this line
+let result = waldoRegex.test(waldoIsHiding)
